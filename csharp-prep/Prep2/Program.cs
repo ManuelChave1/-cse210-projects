0e2803efa3cff9f4a837d881e09cc7cb49e3ed35
Manuel Chave <109537942+ManuelChave1@users.noreply.github.com>
2023-09-14 17:36:40 +0200

    solving prep1 exec

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console . Write("Enter your grade persentege: ");
+        string gradeFromUser = Console.ReadLine();
+        double gradePercentage = double.Parse(gradeFromUser);
+
+
+        if (gradePercentage > 100)
+        {
+            Console.WriteLine("Enter values ​​from 0 to 100.");
+        }
+        else if (gradePercentage >= 90)
+        {
+           Console.WriteLine($"Your grade is A."); 
+        }
+        else if(gradePercentage >= 80)
+        {
+            Console.WriteLine("Your grade is B.");
+        }
+
+        else if(gradePercentage >= 70)
+        {
+            Console.WriteLine("Your grande is C.");
+        } 
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Your grade is D.");
+        }
+        else if (gradePercentage < 60)
+        {
+            Console.WriteLine("Your grade is F.");
+        }
+
+        if (gradePercentage > 100)
+        {
+            Console.WriteLine("Enter values​from 0 to 100");
+        }
+        else if (gradePercentage >=70)
+        {
+            Console.WriteLine("Congratulations you passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you are not aproved, please try next time.");
+        }
     }
 }
