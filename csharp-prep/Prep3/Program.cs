f15cce3e6c34312eb87d08b2f2e224f745bc6e66
Manuel Chave <109537942+ManuelChave1@users.noreply.github.com>
2023-09-22 16:16:41 +0200

    solved all 3 assign for guessing game assignment and other 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,58 @@
 using System;
 
+
 class Program
 {
+  
+  
     static void Main(string[] args)
+      
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+       // Console.WriteLine("Welocme to guessing game!");
+       /* Console.Write("What is the magic number? ");
+        int magicNum = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+      if(guess == magicNum)
+        {
+            Console.Write("You guessed it!");
+        }
+        else if (guess > magicNum)
+        {
+            Console.Write("Lower");
+        }
+        else if (guess < magicNum)
+        {
+            Console.Write("Higher");
+        }
+      */
+        // part 3 assigment
+      Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+     
     }
 }
